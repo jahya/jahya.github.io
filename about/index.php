2268952bd804abc812e25bf59814b109b5ee37e7
Andy McWilliams <andy@jahya.net>
2014-09-25 12:09:41 -0400

    Cleans up 'about' page

--- about/index.php
+++ about/index.php
@@ -13,17 +13,17 @@
 				<div id="detail_img">
 					<img src="/img/about/long1.png" alt="Still from the audio/visual installation Long (2012)" />
 					<p class="imglabel aboutimg">
-						<a href="/portfolio/long">Still from installation Long (2012), at Contemporary Artist Center</a></a>
+						Still from installation <a href="/portfolio/long">Long (2012)</a>, at Contemporary Artist Center</a>
 					</p>
 				</div>
 
 				<h2>Bio</h2>
 
-				<p>Andrew McWilliams is a British-born, New York-based independent artist. In exploring relationships between materiality, sensation and presence, his practice spans installation, sound, light, video and software engineering.</p>
+				<p>Andrew McWilliams is a British-born, New York-based independent artist, technologist, and program director. Exploring relationships between materiality, sensation and presence, his practice spans installation, sound, light, video and software engineering.</p>
 
 				<p>McWilliams has exhibited work at <a href="http://jaaga.in/">Jaaga Creative Common Ground</a> (India), <a href="http://www.alpha-ville.co.uk/">AlphaVille Festival of Post-Digital Culture</a> (UK), <a href="http://www.freemote.nl/">Freemote</a> (Netherlands), and <a href="http://www.thoughtworks.com/">ThoughtWorks</a> (USA). He has been awarded residencies at <a href="http://www.cactroy.org/">Contemporary Artist Center</a> (USA), <a href="http://www.i-park.org/">I-Park Foundation</a> (USA), and on the <a href="http://jaaga.in/residency/">Jaaga Residency Program</a> (India).</p>
 
-				<p>McWilliams is the co-founder of <a href="http://art-a-hack.github.io/">Art-A-Hack</a>, a biannual intensive co-creation program in New York, and co-founder of <a href="http://hardwarehacklab.tumblr.com/">Hardware Hack Lab</a>, a weekly workshop exploring advances in technology. He also co-runs the <a href="http://nyc.volumetric.org">Volumetric Society</a>, which aims to broaden the conversation between technology and the arts, via a program of public presentations and events.</p>
+				<p>McWilliams is founder and director of <a href="http://art-a-hack.github.io/">Art-A-Hack</a>, a biannual co-creation program in New York, and founder and director of <a href="http://hardwarehacklab.tumblr.com/">Hardware Hack Lab</a>, a weekly arts/technology community workshop. He is also a program coordinator with the <a href="http://nyc.volumetric.org">Volumetric Society</a>, which aims to broaden the conversation between technology and the arts through public presentations and events.</p>
 
 				<h2>Statement</h2>
 				<p>I see my work as interventions which undercut the things we take for-granted just by being human. Our daily experience is composed of a continuous, constructed network of mental signs and signifiers which govern our perception. Using technology as a medium, I create sensory environments which seek to question - and alter - how we navigate, make sense of, and relate to the world.</p>
