66d78a1377f4edb0c5123336b7ffef1537c02e0f
Andrew McWilliams <andy@jahya.net>
2015-03-22 18:24:59 -0400

    Removes old site

--- _old site/includes/client-side-script/homepage.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$('#promo_img').jshowoff({
-		controls: false,
-		changeSpeed: 2000,
-		speed: 5000
-	});
-});
--- _old site/portfolio/gravity/index.php
+++
@@ -1,3 +0,0 @@
-<?
-	require("../../works/gravity/index.php");
-?>
