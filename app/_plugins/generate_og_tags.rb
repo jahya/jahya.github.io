89405c6865b7722c94a5d33e46e277c04e982dc8
Andrew McWilliams <andy@jahya.net>
2017-07-13 18:54:30 -0400

    Generates og descriptions for blog posts

---
+++ app/_plugins/generate_og_tags.rb
@@ -0,0 +1,51 @@
+module GenerateOGTags
+  class Generator < Jekyll::Generator
+    def generate(site)
+      generate_og_tags site
+    end
+
+    def generate_og_tags(site)
+      site.posts.each do |post|
+        generate_og_image_tags(post)
+        generate_og_description_tags(post)
+      end
+    end
+
+    def generate_og_image_tags(post)
+      first_image_src = get_first_image_src(post.content)
+
+      if is_usable_image_src(first_image_src) then
+        post.data['image'] = first_image_src
+      end
+    end
+
+    def generate_og_description_tags(post)
+      first_paragraph = get_first_paragraph(post.content)
+
+      if is_usable_paragraph(first_paragraph) then
+        first_paragraph = strip_tags(first_paragraph)
+        post.data['description'] = first_paragraph
+      end
+    end
+
+    def get_first_image_src(content)
+      content[/img.*?src="(.*?)"/i, 1]
+    end
+
+    def get_first_paragraph(content)
+      content[/<p>(.*)<\/p>/i, 1]
+    end
+
+    def is_usable_image_src(src)
+      !src.nil? && src.start_with?('/images/blog/')
+    end
+
+    def is_usable_paragraph(src)
+      !src.nil?
+    end
+
+    def strip_tags(content)
+      content.gsub(/<\/?[^>]*>/, "")
+    end
+  end
+end
